215ecc977b07c1650f3d286413a022867cc21fa7
ssafy11thseoul <ssaf010203@gmail.com>
2024-08-22 11:11:26 +0900

    :bulb: comment: React.createElement로 Greeting 함수 만들기 주석 작성

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -2,8 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greeting() {
-  return <h2> My First Component</h2>;
+  return (
+    <div>
+      <h2> My First Component</h2>
+    </div>
+  );
 }
+
+// React.createElement로 만들기 ... 복잡
+// function Greeting() {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h2", {}, "hello world")
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
