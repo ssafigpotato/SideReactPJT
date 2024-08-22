9589b215f8429e1f3844ba6fab89c1ad0a75e6d7
ssafy11thseoul <ssaf010203@gmail.com>
2024-08-22 15:31:22 +0900

    :sparkles: feat: props 2

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -5,12 +5,12 @@ import bookImage from "./images/book.webp";
 
 const title = "카네기 인간관계론";
 const author = "abcde";
-// const img = "./images/book.webp";
+const img = "./images/book.webp";
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book job="developer" />
-      <Book title="random title" number={22} />
+      <Book author={author} title={title} img={img} />
+      <Book author={author} title={title} img={img} />
     </section>
   );
 };
@@ -19,24 +19,12 @@ const Book = (props) => {
   console.log(props);
   return (
     <article className="book">
-      <img src={bookImage} alt={title} />
-      <h2>{title} </h2>
-      <h4>{author.toUpperCase()}</h4>
-      <p>{props.job}</p>
-      <p>{props.title}</p>
-      <p>{props.number}</p>
+      <img src={bookImage} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
-
-// React.createElement로 만들기 ... 복잡
-// function Greeting() {
-//   return React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h2", {}, "hello world")
-//   );
-// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
