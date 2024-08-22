09a49401169ec2349c4b0c58b75e658d1cec7f57
ssafy11thseoul <ssaf010203@gmail.com>
2024-08-22 16:35:28 +0900

    :sparkles: feat: props - dynamic setup

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -3,14 +3,30 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import bookImage from "./images/book.webp";
 
-const title = "카네기 인간관계론";
-const author = "abcde";
-const img = "./images/book.webp";
+const firstBook = {
+  author: "카네기",
+  title: "카네기 인간관계론",
+  img: "./images/book.webp",
+};
+const secondBook = {
+  author: "탈무드",
+  title: "탈무드 Talmud",
+  img: "https://m.media-amazon.com/images/I/31QC9mAKVnL._AC_UL582_FMwebp_QL65_.jpg",
+};
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
@@ -19,7 +35,7 @@ const Book = (props) => {
   console.log(props);
   return (
     <article className="book">
-      <img src={bookImage} alt={props.title} />
+      <img src={props.img} alt={props.title} />
       <h2>{props.title}</h2>
       <h4>{props.author}</h4>
     </article>
