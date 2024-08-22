ecd396d3fbf45155f7ca04976591dbaae53fd4fe
ssafy11thseoul <ssaf010203@gmail.com>
2024-08-22 11:04:12 +0900

    :bulb: comment: testing.js파일 주석 추가

--- my-react/src/testing.js
+++ my-react/src/testing.js
@@ -1,4 +1,6 @@
-import React from "react";
+// import React from "react";
+// 상위에서 작성했다면 생략가능. 17부터는 모든 컴포넌트에서 생략가능
+// 중복으로 써도 웹팩을 통해 최적화된 코드로 작성되므로 비효율성은 걱정 노노
 
 function Testing() {
   return <div>testing</div>;
