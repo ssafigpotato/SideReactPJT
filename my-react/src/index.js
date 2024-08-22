8e8a1c27b6e7dc15b12beeade5cb093e7a422ce1
ssafy11thseoul <ssaf010203@gmail.com>
2024-08-22 13:05:18 +0900

    :sparkles: feat: nest component 생성

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -5,20 +5,16 @@ function Greeting() {
   return (
     // 부모 요소 하나만 반환해야함
     <>
-      <div className="something">
-        <h3> hello people</h3>
-        <ul>
-          <li>
-            <a href="#">hello world</a>
-          </li>
-        </ul>
-      </div>
-      <h3>부모요소는 하나만 반환해야함</h3>
-      <input type="text" name="" id="" />
+      <Person />
+      <Message />
     </>
   );
 }
 
+const Person = () => <h2>John doe</h2>;
+const Message = () => {
+  return <h2>This is my message</h2>;
+};
 // React.createElement로 만들기 ... 복잡
 // function Greeting() {
 //   return React.createElement(
