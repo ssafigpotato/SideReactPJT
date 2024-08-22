210cc679dba81161afd080670392891ba70df00b
ssafy11thseoul <ssaf010203@gmail.com>
2024-08-22 15:09:09 +0900

    :sparkles: feat: JSX Javascript

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -13,30 +13,21 @@ const BookList = () => {
   );
 };
 
+const author = "abcde";
 const Book = () => {
+  const title = "카네기 인간관계론";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/61wszoCLDOL._AC_UL640_FMwebp_QL65_.jpg"
+        alt="카네기 뭐시기 책"
+      />
+      <h2>{title} </h2>
+      <h4>{author.toUpperCase()}</h4>
+      {/* {}안에는 값을 반환하는 식만 전달가능 
+      따라서 6+6 은 되지만 let x = 6 이런건 안됨*/}
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/61wszoCLDOL._AC_UL640_FMwebp_QL65_.jpg"
-    alt="카네기 뭐시기 책"
-  />
-);
-const Title = () => <h2>카네기 인간관계론 </h2>;
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-  return <h4 style={inlineHeadingStyles}>카네기</h4>;
 };
 
 // React.createElement로 만들기 ... 복잡
