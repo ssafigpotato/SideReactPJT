fcc101411d26d4fe562d5d628ba4e94216322e8a
ssafy11thseoul <ssaf010203@gmail.com>
2024-08-22 17:30:05 +0900

    :sparkles: feat: names배열 예시

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -3,54 +3,38 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import bookImage from "./images/book.webp";
 
-const firstBook = {
-  author: "카네기",
-  title: "카네기 인간관계론",
-  img: "./images/book.webp",
-};
-const secondBook = {
-  author: "탈무드",
-  title: "탈무드 Talmud",
-  img: "https://m.media-amazon.com/images/I/31QC9mAKVnL._AC_UL582_FMwebp_QL65_.jpg",
-};
+const books = [
+  {
+    author: "카네기",
+    title: "카네기 인간관계론",
+    img: "./images/book.webp",
+  },
+  {
+    author: "탈무드",
+    title: "탈무드 Talmud",
+    img: "https://m.media-amazon.com/images/I/31QC9mAKVnL._AC_UL582_FMwebp_QL65_.jpg",
+  },
+];
 
+const names = ["김싸피", "이싸피", "박싸피"];
+const newNames = names.map((name) => {
+  console.log(name);
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Ea in consequat ipsum magna eiusmod exercitation eiusmod enim cillum.
-          In nostrud incididunt tempor dolore et voluptate aute esse sunt.
-          Tempor ullamco reprehenderit aute aute deserunt minim esse eu proident
-          aliqua. Eiusmod eu officia id eiusmod nulla ea et velit. Sit amet ad
-          ullamco nostrud id dolore aliquip nulla est tempor in non esse.
-          Laborum sunt laboris voluptate sunt consectetur esse ex quis consequat
-          nulla occaecat commodo magna.
-        </p>
-        <button>Click me!</button>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 };
 
 const Book = (props) => {
-  const { img, author, title, children } = props;
   console.log(props);
+  const { img, author, title, children } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <h4>{author}</h4>
       {children}
+      <h4>{author}</h4>
     </article>
   );
 };
